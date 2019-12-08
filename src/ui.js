dd845d12017b26eca648d70525a7e31cb3ca2775
ndvo <ndvo@pop-os.localdomain>
2019-12-08 15:17:39 -0800

    Continue removing unused functions

--- src/ui.js
+++ src/ui.js
@@ -17,10 +17,6 @@ export class UISelectors {
     static errorMessage = ".error-message";
 }
 
-async function delay(ms) {
-    // return await for better async stack trace support in case of errors.
-    return await new Promise(resolve => setTimeout(resolve, ms));
-}
 
 class UICtrl {
 
